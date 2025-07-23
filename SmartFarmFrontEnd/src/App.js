dd7c9b24ec6f2afb9c55ff7368777b68894e64e2
seuntwoosoon <tnddm0927@naver.com>
2025-07-23 15:53:08 +0900

    fix: SmartFarmFrontEnd를 일반 폴더로 변경

---
+++ SmartFarmFrontEnd/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((res) => res.text())
+      .then((data) => setMessage(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>🧪 Test React + Spring Boot</h1>
+      <p>🚀 백엔드 응답: {message}</p>
+    </div>
+  );
+}
+
+export default App;
