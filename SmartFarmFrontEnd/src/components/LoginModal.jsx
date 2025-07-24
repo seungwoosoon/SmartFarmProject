c6d79b1a9f2f536d1c6692872e22ca10b2e1a62a
chanbin <rcq0111@gmail.com>
2025-07-24 19:13:53 +0900

    frontend v0.2

--- SmartFarmFrontEnd/src/components/LoginModal.jsx
+++ SmartFarmFrontEnd/src/components/LoginModal.jsx
@@ -1,1 +1,32 @@
 /* 로그인 모달 */
+
+import React from 'react';
+import '../App.css'; // 또는 Modal 전용 CSS로 따로 분리 가능
+
+function LoginModal({ onClose, onSwitchToSignup }) {
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+
+        <h2 className="modal-title">LOG - IN</h2>
+
+        <div className="login-input-group">
+          <label>ID</label>
+          <input type="text" placeholder="아이디 입력" />
+          <label>PW</label>
+          <input type="password" placeholder="비밀번호 입력" />
+        </div>
+
+        <button className="login-btn-modal">LOG IN</button>
+
+        <p className="switch-links" onClick={onSwitchToSignup}>
+          FORGOT LOGIN / SIGN - UP
+        </p>
+
+        <button className="close-btn" onClick={onClose}>X</button>
+      </div>
+    </div>
+  );
+}
+
+export default LoginModal;
