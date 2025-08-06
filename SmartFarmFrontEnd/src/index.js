e5853e9c6f5f98a0dba3a0f106093d8c6c2ac459
chanbin <rcq0111@gmail.com>
2025-08-06 17:16:12 +0900

    frontend_v3 v0.3

--- SmartFarmFrontEnd/src/index.js
+++ SmartFarmFrontEnd/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios'; // ✅ 추가
+axios.defaults.withCredentials = true; // ✅ 모든 요청에 세션 쿠키 포함
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
