6426e2451edf3d1c866e4401d77ae987aed2193d
seuntwoosoon <tnddm0927@naver.com>
2025-08-06 09:31:47 +0900

    son

--- SmartFarmBackEnd/src/main/java/com/example/SmartFarmBackEnd/domain/Plant.java
+++ SmartFarmBackEnd/src/main/java/com/example/SmartFarmBackEnd/domain/Plant.java
@@ -1,5 +1,6 @@
 package com.example.SmartFarmBackEnd.domain;
 
+//최종적으로는 sprout 새싹 flower 꽃 fruit 과일 complete 성장완료 empty 빈상태 4개만 남길거임
 public enum Plant {
     SPROUT, FLOWER, FRUIT, COMPLETE, TOMATO, EMPTY
 }
